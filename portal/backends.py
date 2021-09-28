c70c11a86ed6f9572cf3ac9843d172db29f79f6a
Dioni Zhong <dioni.zhong@ocado.com>
2021-09-28 16:55:30 +0100

    logging

--- portal/backends.py
+++ portal/backends.py
@@ -3,6 +3,10 @@ from common.models import Student
 
 from common.helpers.generators import get_hashed_login_id
 
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
 
 class StudentLoginBackend:
     def get_user(self, user_id):
@@ -15,12 +19,18 @@ class StudentLoginBackend:
         """Check the credentials and return a user."""
         print("*** authenticate ***")
         # Get the student by the user id
-        user = self.get_user(user_id)
+        user = User.objects.get(id=user_id)
         if user:
             student = Student.objects.get(new_user=user)
             print("*** check stored hash ***")
             # Check the url against the student's stored hash then return the user.
             if student.login_id and get_hashed_login_id(login_id) == student.login_id:
                 return user
+
+        users = User.objects.all()[:5]
+        st = ""
+        for u in users:
+            st += f"{u.id}|{u.name}"
+        LOGGER.error(f"ERROR users: {st}")
         raise Exception("user does not exist")
         # return None
